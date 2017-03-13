1f7e898d90edd529243979a140b9fb62f4128efb
eyolas <dtouzet@gmail.com>
2017-03-13 13:23:51 +0100

    add lint

--- packages/typeorm/src/pluginTypeormConfig.ts
+++ packages/typeorm/src/pluginTypeormConfig.ts
@@ -1,6 +1,6 @@
 import { PluginConfig, Value, optional, Bean, CORE_CONFIG, inject, interfaces } from '@gabliam/core';
 import { ConnectionOptions, Connection, createConnection } from './typeorm';
-import {ConnectionOptionsBeanId} from './constant';
+import { ConnectionOptionsBeanId } from './constant';
 import * as d from 'debug';
 
 const debug = d('Gabliam:Plugin:Typeorm');
@@ -26,9 +26,9 @@ export class PluginTypeormConfig {
     @Bean(Connection)
     create() {
         debug('connectionOptions', this.connectionOptions);
-        if (!this.connectionOptions){
+        if (!this.connectionOptions) {
             throw new Error(`PluginTypeormConfig connectionOptions is mandatory`);
-        } 
+        }
         let connectionOptions = this.connectionOptions;
         let entities: any = connectionOptions.entities || [];
         entities.push(this.entitiesPath);
