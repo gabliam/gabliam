e09d1a98e2acb8af9869c4f6326a99422e16c250
eyolas <dtouzet@gmail.com>
2017-04-24 13:53:13 +0200

    update for gabliam v2

--- packages/typeorm/src/index.ts
+++ packages/typeorm/src/index.ts
@@ -1,5 +1,4 @@
 import { interfaces, Scan, inversifyInterfaces, Registry } from '@gabliam/core';
-import * as express from 'express';
 import { Connection } from './typeorm';
 
 export * from './typeorm';
@@ -8,7 +7,7 @@ export * from './constant';
 
 @Scan(__dirname)
 export default class PluginsTypeOrm implements interfaces.GabliamPlugin {
-    async destroy(app: express.Application, container: inversifyInterfaces.Container, registry: Registry) {
+    async destroy(container: inversifyInterfaces.Container, registry: Registry) {
         let connection = container.get<Connection>(Connection);
         await connection.close();
     }
