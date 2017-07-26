87af177782952d4d1cd7bbf91e0a68dbdfa4ab3b
eyolas <dtouzet@gmail.com>
2017-07-26 14:39:35 +0200

    chore(package): update deps

--- packages/express/__tests__/express-plugin-test.ts
+++ packages/express/__tests__/express-plugin-test.ts
@@ -1,60 +1,22 @@
 import { Gabliam } from '@gabliam/core';
+import { GabliamTest } from '@gabliam/core/lib/testing';
 import * as e from 'express';
 import * as path from 'path';
-import { METADATA_KEY } from '@gabliam/core/lib/constants';
-import { Loader as CoreLoader } from '@gabliam/core/lib/loader';
-import { RegistryMetada } from '@gabliam/core/lib/interfaces';
 import ExpressPlugin, { APP } from '../src/index';
-import * as _ from 'lodash';
 
-export class Loader extends CoreLoader {
-  conf = {};
-
-  addConfig(p: string, conf: any) {
-    this.conf = _.set(this.conf, p, conf);
-  }
-
-  loadConfig(folder: string, profile?: string | null): any {
-    const config = super.loadConfig(folder, profile);
-    return _.merge({}, config, this.conf);
-  }
-}
-
-export class ExpressPluginTest {
-  public gab: Gabliam;
+export class ExpressPluginTest extends GabliamTest {
   public app: e.Application;
 
-  public loader: Loader;
-
   constructor() {
-    this.gab = new Gabliam({
-      scanPath: path.resolve(__dirname, './decorators/__snapshots__'),
-      configPath: path.resolve(__dirname, './decorators/__snapshots__')
+    const gab = new Gabliam({
+      scanPath: path.resolve(__dirname, 'gabliam'),
+      configPath: path.resolve(__dirname, 'gabliam')
     }).addPlugin(ExpressPlugin);
-    this.loader = (<any>this.gab)._loader = new Loader();
+    super(gab);
   }
 
   async start() {
-    await this.gab.build();
+    await super.start();
     this.app = this.gab.container.get<e.Application>(APP);
-  }
-
-  async destroy() {
-    await this.gab.destroy();
-  }
-
-  addConf(p: string, conf: any) {
-    this.loader.addConfig(p, conf);
-  }
-
-  addClass(ctrl: any) {
-    if (Reflect.hasOwnMetadata(METADATA_KEY.register, ctrl)) {
-      const metadata = <RegistryMetada>Reflect.getOwnMetadata(
-        METADATA_KEY.register,
-        ctrl
-      );
-      this.gab.registry.add(metadata.type, metadata.value);
-    }
-    return this;
   }
 }
