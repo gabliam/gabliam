792f12e555b606065fb89f0eb95ea64bd7c7c098
eyolas <dtouzet@gmail.com>
2019-01-10 11:56:23 +0100

    feat(@gabliam/typeorm): upgrade typeorm

--- packages/typeorm/__tests__/decorators.test.ts
+++ packages/typeorm/__tests__/decorators.test.ts
@@ -1,13 +1,9 @@
 import { RegistryMetada } from '@gabliam/core/lib/interfaces';
-import {
-  Entity,
-  ClassEntityChild,
-  ClosureEntity,
-  SingleEntityChild,
-} from '../src/decorators';
+import { Entity, ChildEntity, CUnit } from '../src/decorators';
 import { METADATA_KEY as CORE_METADATA_KEY } from '@gabliam/core/lib/constants';
+import { METADATA_KEY } from '../src/constant';
 
-[Entity, ClassEntityChild, ClosureEntity, SingleEntityChild].forEach(deco => {
+[Entity, ChildEntity].forEach(deco => {
   test(`${deco.name} decorators`, () => {
     @deco()
     class TestEntity {}
@@ -18,5 +14,30 @@ import { METADATA_KEY as CORE_METADATA_KEY } from '@gabliam/core/lib/constants';
     );
 
     expect(entityMetadata).toMatchSnapshot();
+  });
+});
+
+describe('CUnit decorators', () => {
+  test('should add CUnit metadata to a class when decorating a method with @CUnit', () => {
+    @CUnit('default')
+    class TestEntity {}
+
+    const entityMetadata: RegistryMetada = Reflect.getMetadata(
+      METADATA_KEY.cunit,
+      TestEntity
+    );
+
+    expect(entityMetadata).toMatchSnapshot();
+  });
+
+  test('should fail when decorated multiple times with @CUnit', () => {
+    expect(function() {
+      @CUnit('default')
+      @CUnit('default2')
+      class TestBean {}
+
+      // tslint:disable-next-line:no-unused-expression
+      new TestBean();
+    }).toThrowError();
   });
 });
